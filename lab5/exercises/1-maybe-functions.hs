cce0a0b5ae34864769660d4a22a6902ceace9800
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-04-14 10:34:16 +0200

    [lab5] add more solutions to the first exercise

--- lab5/exercises/1-maybe-functions.hs
+++ lab5/exercises/1-maybe-functions.hs
@@ -1,22 +1,32 @@
+module MaybeFunctions where
+
 import Data.Char
+import Control.Monad
+import Control.Applicative
 
 -- Common functions
+-- isLowercase could be substitued for Data.Char.isLower
 isLowercase x = asciiNum >= 97 && asciiNum <= 122
   where
     asciiNum = ord x
 
 capitalizeSingleLetter :: Char -> Maybe Char
 capitalizeSingleLetter letter
-| isLowercase letter = Just $ toUpper letter
-| otherwise = Nothing
+  | isLowercase letter = Just $ toUpper letter
+  | otherwise = Nothing
 
 -- Pattern matching only
 capitalize :: String -> Maybe String
-capitalize s = mapM capitalizeSingleLetter s
+capitalize "" = Just ""
+capitalize (x:xs) = case capitalizeSingleLetter x of
+  Just x' -> case capitalize xs of
+    Just xs' -> Just (x':xs')
+    Nothing -> Nothing
+  Nothing -> Nothing
 
-capitalize' :: Maybe String -> Maybe String
-capitalize' Nothing = Nothing
-capitalize' (Just x) = capitalize x
+capitalizeMaybe :: Maybe String -> Maybe String
+capitalizeMaybe Nothing = Nothing
+capitalizeMaybe (Just x) = capitalize x
 
 unaryNumber :: Int -> Maybe String
 unaryNumber x
@@ -27,13 +37,43 @@ unaryNumber x
       | x > 0 = "0" ++ numToZeros (x - 1)
       | otherwise = ""
 
+unaryNumberMaybe :: Maybe Int -> Maybe String
+unaryNumberMaybe Nothing = Nothing
+unaryNumberMaybe (Just x) = unaryNumber x
+
 interleave :: String -> String -> String
 interleave "" _ = ""
 interleave _ "" = ""
 interleave (a:as) (b:bs) = a:b:(interleave as bs)
 
-interleave' :: Maybe String -> Maybe String -> Maybe String
-interleave' Nothing _ = Nothing
-interleave' _ Nothing = Nothing
-interleave' (Just x) (Just y) = interleave x y
+interleaveMaybe :: Maybe String -> Maybe String -> Maybe String
+interleaveMaybe Nothing _ = Nothing
+interleaveMaybe _ Nothing = Nothing
+interleaveMaybe (Just x) (Just y) = Just $ interleave x y
 
+-- Using Monad operators
+capitalize' :: String -> Maybe String
+capitalize' s = mapM capitalizeSingleLetter s
+
+capitalizeMaybe' :: Maybe String -> Maybe String
+capitalizeMaybe' = join . liftM capitalize'
+
+-- No idea how to make unaryNumber with monad operators
+
+-- interleave using monads does not make sense since it operates on strings
+interleaveMaybe' :: Maybe String -> Maybe String -> Maybe String
+interleaveMaybe' = liftM2 interleave
+
+-- Using Applicative operators
+capitalize'' :: String -> Maybe String
+capitalize'' s = sequenceA $ fmap capitalizeSingleLetter s
+
+-- join is monadic, so I guess it's a bit of a cheat
+capitalizeMaybe'' :: Maybe String -> Maybe String
+capitalizeMaybe'' = join . liftA capitalize''
+
+unaryNumber'' :: Int -> Maybe String
+unaryNumber'' x
+  | x == 0 = Just ""
+  | x < 0 = Nothing
+  | otherwise = ('0':) <$> unaryNumber (x-1)
