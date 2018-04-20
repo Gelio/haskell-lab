9f6f63500103749d87fb9504845100c84ddce703
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-04-20 07:52:40 +0200

    half-working tic tac toe

--- tic-tac-toe/src/Main.hs
+++ tic-tac-toe/src/Main.hs
@@ -1,9 +1,10 @@
 module Main where
 
-import Data.Sequence as S
+import qualified Data.Sequence as S
 import Control.Monad
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State.Strict
+import Text.Read (readMaybe)
 
 data Token = X | O deriving (Show)
 
@@ -18,19 +19,39 @@ putToken t pos
   | pos < 1 || pos > 9 = return False
   | otherwise = modify' (S.update (pos-1) (Just t)) >> return True
 
+winningIndices = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 5, 8], [3, 4, 5], [6, 7, 8], [2, 4, 6]]
+
+checkWinningIndexSet :: Board -> [Int] -> Maybe Token
+checkWinningIndexSet board indices = sequence tokens >>= \t -> if all (== head t) t then return $ head t else Nothing
+  where
+    tokens :: [Maybe Token]
+    tokens = fmap indices (`S.lookup` board)
+
+isWin :: TTTState (Maybe Token)
+isWin = do
+  board <- get
+  let boardWins = filter (checkWinningIndexSet board) winningIndices
+    in
+      case boardWins of
+        [] -> return Nothing
+        (x:xs) -> return x
+
 displayBoard :: TTTState ()
 displayBoard = do
   board <- get
   lift $ print board
 
+ask :: String -> IO String
+ask s = putStrLn s >> getLine
+
+readInt :: String -> Maybe Int
+readInt = readMaybe
+
+playGame :: TTTState ()
+playGame = do
+  index <- lift $ ask "Index between 1 and 9"
+  maybe (lift $ putStrLn "Index should be a number") (\i -> putToken X i >> displayBoard >> isWin >>= \w -> when w (lift $ putStrLn "Win!")) (readInt index)
+  playGame
 
 main :: IO ()
-main = void $ execStateT (do
-    result <- putToken X 1
-    if result then
-      lift $ putStrLn "Yes"
-    else
-      lift $ putStrLn "No"
-    displayBoard
-    return ()
-  ) emptyBoard
+main = void $ execStateT playGame emptyBoard
