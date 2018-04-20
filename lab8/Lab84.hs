0f87420ae6ec9dffaff118ae0fe6902d8610b376
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-04-20 09:59:48 +0200

    [lab8] complete task 4

--- lab8/Lab84.hs
+++ lab8/Lab84.hs
@@ -1,13 +1,12 @@
 module Task4 where
 
-import Control.Monad
-
 distSquared :: Int -> Int -> Int
 distSquared x y = (x - y)^2
 
 distance3 :: (Int, Int, Int) -> (Int, Int, Int) -> Double
 distance3 (x1, y1, z1) (x2, y2, z2) = sqrt $ fromIntegral $ (distSquared x1 x2) + (distSquared y1 y2) + (distSquared z1 z2)
 
+-- Rozwiązanie z list comprehension
 punktyWkuli :: (Int, Int, Int) -> Int -> [(Int, Int, Int)]
 punktyWkuli c@(cx, cy, cz) r = [p |
   x <- xRange, y <- yRange, z <- zRange,
@@ -17,3 +16,20 @@ punktyWkuli c@(cx, cy, cz) r = [p |
     xRange = [(cx - r)..(cx + r)]
     yRange = [(cy - r)..(cy + r)]
     zRange = [(cz - r)..(cz + r)]
+
+-- Drugie rozwiązanie, przekształcenie pierwszego na notację bez list comprehension
+getAllPoints :: (Int, Int, Int) -> Int -> [(Int, Int, Int)]
+getAllPoints (cx, cy, cz) r = do
+  x <- xRange
+  y <- yRange
+  z <- zRange
+
+  return (x, y, z)
+  where
+    xRange = [(cx - r)..(cx + r)]
+    yRange = [(cy - r)..(cy + r)]
+    zRange = [(cz - r)..(cz + r)]
+
+punktyWkuli' :: (Int, Int, Int) -> Int -> [(Int, Int, Int)]
+punktyWkuli' c r = filter (\p -> distance3 c p <= fromIntegral r) points
+  where points = getAllPoints c r
